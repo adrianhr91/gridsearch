56403d780d812a646e29a093fb4d31d77a9ec05d
Adrian Hristov <adrianhr91@gmail.com>
2016-02-07 19:52:30 +0000

    Added web project

---
+++ GridSearch.Web/app/app.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular
+        .module('gridsearch', [
+                'ngRoute',
+                'gridsearch.controllers'
+        ]);
+
+    app.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    redirectTo: '/nearby'
+                })
+                .when('/nearby', {
+                    templateUrl: 'search/search.partial.html',
+                    controller: 'NearbyController',
+                    controllerAs: 'NearbyCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/nearby'
+            });
+        }]);
+
+    (function () {
+        'use strict';
+        angular
+            .module('gridsearch.controllers', []);
+    })();
+})();
---
+++ GridSearch.Web/app/search/nearbyController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('gridsearch.controllers')
+        .controller('NearbyController', NearbyController);
+
+    NearbyController.$inject = [];
+
+    function NearbyController() {
+
+        var nearbyCtrl = this;
+    }
+})();
